5dd2a97cc71bd27cbf66075730f21e30209d11f0
vipendra <vipendrasinghpatel999@gmail.com>
2026-02-11 20:43:44 +0530

    Add everything

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,9 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 // env variables
 const PORT = process.env.PORT || 8000;
 
-console.log(process.env.NODE_ENV);
-
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  logger.info(`Server is listening on port ${PORT}`);
 });
