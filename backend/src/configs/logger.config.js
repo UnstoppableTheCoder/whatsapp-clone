ca67c303eaf37209c61a4f01230a0c9cd47d9218
vipendra <vipendrasinghpatel999@gmail.com>
2026-02-11 20:42:46 +0530

    Custom logger message

---
+++ backend/src/configs/logger.config.js
@@ -0,0 +1,45 @@
+import winston from "winston";
+
+// Dive deep into it
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "development" ? "debug" : "info",
+
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    process.env.NODE_ENV === "development"
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+
+    winston.format.splat(),
+
+    winston.format.printf(({ level, message }) => {
+      return `${level}: ${message}`;
+    }),
+  ),
+
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ["error"],
+    }),
+  ],
+});
+
+export default logger;
+
+// Levels in docs ->
+// const levels = {
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   http: 3,
+//   verbose: 4,
+//   debug: 5,
+//   silly: 6,
+// };
