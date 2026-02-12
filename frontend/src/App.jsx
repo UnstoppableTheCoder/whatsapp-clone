b6451aa05307fe4df7863ff5269a8401751c040b
vipendra <vipendrasinghpatel999@gmail.com>
2026-02-12 12:02:28 +0530

    Setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChatIcon } from "./svg";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to the frontend</h1>
+    </div>
+  );
+};
+
+export default App;
